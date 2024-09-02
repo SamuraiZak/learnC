1c12cc23c8299d5cfeadb13e24f27483b8da4942
mohd.zaki <mohd.zaki@impact-multimedia.com>
2024-09-02 15:20:54 +0800

    commit

---
+++ c_files/HelloWorld.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    // char name[] = "zaki";
+    // int age = 24;
+    // float gpa = 3.33;
+    // char singleCharacter = 'Z';
+
+    // printf("%s (%c) is %d years old and graduated with cgpa %0.2f", name, singleCharacter, age, gpa);
+
+    return 0;
+}
